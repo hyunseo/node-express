aa1fe36f112377dcef59d3e4963a1a7208efe6f4
hyunseo <starno1@empal.com>
2023-11-15 14:51:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 
+const static = require('node-static'); 
+const http = require('http');
+const socketIO = require('socket.io');
+const express = require("express");
+
+//const { WebSocketServer } = require("ws");
+const file = new static.Server('./client');
+const app = http
+  .createServer(file.serve.bind(file))
+  .listen(3000, () => {
+	console.log(`sample app listening on port 3000`)
+})
